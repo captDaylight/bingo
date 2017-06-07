9dd3c8086ca782b1609af04c0fb9e22bda1f25e0
paul <paul.christophe6@gmail.com>
2017-06-07 18:59:10 -0400

    selecting squares correctly

--- src/Bingo.js
+++ src/Bingo.js
@@ -2,7 +2,30 @@ import React, { Component } from 'react'
 import Board from './components/Board'
 
 class Bingo extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      rowAndColumn: ''
+    }
+
+    this.selectSquare = this.selectSquare.bind(this)
+  }
+
+  selectSquare(_rowAndColumn) {
+    console.log('selecting square', _rowAndColumn);
+    const { rowAndColumn } = this.state
+
+    if (rowAndColumn === _rowAndColumn) {
+      return this.setState({ rowAndColumn: '' })
+    }
+
+    this.setState({ rowAndColumn: _rowAndColumn })
+  }
+
   render() {
+    const { rowAndColumn } = this.state
+
     return (
       <div>
         <header>
@@ -10,7 +33,7 @@ class Bingo extends Component {
           <p>Select a square to get started</p>
         </header>
 
-        <Board />
+        <Board rowAndColumn={rowAndColumn} selectSquare={this.selectSquare} />
       </div>
     )
   }
--- src/components/Board.js
+++ src/components/Board.js
@@ -5,13 +5,18 @@ const columns = [ '1', '2', '3' ]
 
 class Board extends Component {
   render() {
+    const { selectSquare, rowAndColumn } = this.props
+
     return (
       <div>
         {rows.map((r, i) => {
           return <ul className="bingo-row" key={`${r}-${i}`}>
             {columns.map((c, i) => {
               return <li className="bingo-column" key={`${c}-${i}`}>
-                <div className="bingo-item">
+                <div
+                  className={`bingo-item ${`${r}-${c}` === rowAndColumn ? 'selected' : ''}`}
+                  onClick={() => selectSquare(`${r}-${c}`) }
+                >
                   <div className="bingo-text">{r} {c}</div>
                 </div>
               </li>
