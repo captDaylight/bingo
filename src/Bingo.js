c020b9918ecbe965f46a48336dc1b27ee5b3b887
paul <paul.christophe6@gmail.com>
2017-06-07 19:17:35 -0400

    confirmation to purchase square

--- src/Bingo.js
+++ src/Bingo.js
@@ -13,7 +13,7 @@ class Bingo extends Component {
   }
 
   selectSquare(_rowAndColumn) {
-    console.log('selecting square', _rowAndColumn);
+    console.log('selecting square', typeof _rowAndColumn);
     const { rowAndColumn } = this.state
 
     if (rowAndColumn === _rowAndColumn) {
@@ -25,12 +25,21 @@ class Bingo extends Component {
 
   render() {
     const { rowAndColumn } = this.state
-
+    console.log('rowAndColumn:', rowAndColumn.length);
     return (
       <div>
         <header>
           <h1 className="header">CHICKEN SHIT BINGO</h1>
-          <p>Select a square to get started</p>
+          {
+            rowAndColumn.length === 0
+            ? <p>Select a square</p>
+            : <p>
+              Purchase?
+              <span className="confirm">YES</span>
+              <span className="confirm" onClick={() => this.selectSquare('')}>NO</span>
+            </p>
+          }
+
         </header>
 
         <Board rowAndColumn={rowAndColumn} selectSquare={this.selectSquare} />
